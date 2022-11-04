9d32150243aa875545e7175fa68a34ea1fd5c91a
DominikLudwiczak <dominik.ludwiczak.priv@gmail.com>
2022-11-04 21:48:31 +0100

    add_migrations

---
+++ hackatonAPI/database/migrations/2022_11_04_204119_products_attributes.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ProductsAttributes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products_attributes', function (Blueprint $table) {
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('attribute_id');
+        });
+
+        Schema::table('products_attributes', function (Blueprint $table){
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products');
+        });
+
+        Schema::table('products_attributes', function (Blueprint $table){
+            $table->foreign('attribute_id')
+                ->references('id')
+                ->on('attributes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products_attributes');
+    }
+}
---
+++ hackatonAPI/database/migrations/2022_11_04_204714_list_entries.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ListEntries extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('list_entries', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('entry_id');
+        });
+
+        Schema::table('list_entries', function (Blueprint $table){
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+
+        Schema::table('list_entries', function (Blueprint $table){
+            $table->foreign('entry_id')
+                ->references('id')
+                ->on('entries');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('list_entries');
+    }
+}
