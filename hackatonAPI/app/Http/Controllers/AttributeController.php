63efeeaa7f5b9acda283b9f99c1e5821fc68c966
DominikLudwiczak <dominik.ludwiczak.priv@gmail.com>
2022-11-05 10:37:44 +0100

    add type into attributes

--- hackatonAPI/app/Http/Controllers/AttributeController.php
+++ hackatonAPI/app/Http/Controllers/AttributeController.php
@@ -5,11 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Attribute;
+use App\Models\Type;
 
 class AttributeController extends Controller
 {
     public function indexAttribute() {
+        $result = [];
         $attributes = Attribute::all();
-        return response()->json(['attributes' => $attributes], 200);
+        foreach($attributes as $attr)
+        {
+            $type = Type::find($attr->type_id);
+
+            $result[] = [
+                'id' => $attr->id,
+                'name' => $attr->name,
+                'type' => $type,
+                'is_required' => $attr->is_required
+            ];
+        }
+        return response()->json(['attributes' => $result], 200);
     }
 }
