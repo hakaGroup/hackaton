d441d1b3d289e96855908a73d718e84b003086e1
Tymon D <tymon.dydowicz@onet.pl>
2022-11-05 02:40:28 +0100

    add links

--- hackatonWEB/src/app/app.module.ts
+++ hackatonWEB/src/app/app.module.ts
@@ -17,6 +17,9 @@ import { NavigationComponent } from './pages/main/navigation/navigation.componen
 import { UserComponent } from './pages/main/user/user.component';
 import { LinksComponent } from './pages/main/links/links.component';
 import { CommonModule } from '@angular/common';
+import { FridgeComponent } from './pages/fridge/fridge.component';
+import { GroceryListComponent } from './pages/grocery-list/grocery-list.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 
 const InitializeConfig = (appConfig: ConfigurationService) => {
@@ -34,7 +37,10 @@ const InitializeConfig = (appConfig: ConfigurationService) => {
     DashboardComponent,
     NavigationComponent,
     UserComponent,
-    LinksComponent
+    LinksComponent,
+    FridgeComponent,
+    GroceryListComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
---
+++ hackatonWEB/src/app/pages/fridge/fridge.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-fridge',
+  templateUrl: './fridge.component.html',
+  styleUrls: ['./fridge.component.css']
+})
+export class FridgeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
