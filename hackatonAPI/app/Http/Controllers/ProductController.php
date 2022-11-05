9e2d83b3d8ef6a473f1c159eff5508acd8755795
ErykWalter <erykwalter2002@gmail.com>
2022-11-05 01:42:23 +0100

    added missing dollar sign in product controller

--- hackatonAPI/app/Http/Controllers/ProductController.php
+++ hackatonAPI/app/Http/Controllers/ProductController.php
@@ -17,7 +17,7 @@ class ProductController extends Controller
 
             if($attributes == $request->attributes)
             {
-                found = True;
+                $found = True;
             }
         }
 
