e46f9f200424d71a1905e11698f8800f33993053
ErykWalter <erykwalter2002@gmail.com>
2022-11-05 01:28:31 +0100

    adding product controller, adding routes to controller functions

---
+++ hackatonAPI/app/Http/Controllers/ProductController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Product;
+use App\Models\Products_attribute;
+
+class ProductController extends Controller
+{
+    public function addProduct (Request $request) {
+        $found = False;
+        $product = Product::where('name', $request->name)->first();
+        if ($product) {
+            $attributes = Products_attribute::where('prod_id', $product->id())->get();
+
+            if($attributes == $request->attributes)
+            {
+                found = True;
+            }
+        }
+
+        if(!$found)
+        {
+            $newProduct = new Product();
+            $newProduct->name = $request->name;
+            $newProduct->save();
+
+
+            foreach ($request->attributes as $attribute) {
+                $newProductAttributes = new Products_attribute();
+                $newProductAttributes->prod_id = $newProduct->id;
+                $newProductAttributes->attr_id = $attribute->id;
+                $newProductAttributes->save();
+            }
+        }
+    }
+
+    public function getProduct ($id) {
+        $product = Product::find($id);
+        if ($product)
+        {
+            return response()->json(['product' => $product], 200);
+        }
+        else
+        {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+    }
+
+    public function indexProduct () {
+        return Product::all();
+    }
+}
--- hackatonAPI/routes/api.php
+++ hackatonAPI/routes/api.php
@@ -4,7 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+
 use App\Models\User;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +24,11 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::get('/user', function(Request $request){
         return $request->user();
     });
-    
+
     Route::get('/islogged', [AuthController::class, 'islogged']);
     Route::post('/user/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::post('/product/add', [ProductController::class, 'addProduct']);
+    Route::get('/product/get/{id}', [ProductController::class, 'getProduct']);
+    Route::get('/product/index', [ProductController::class, 'indexProduct']);
 });
